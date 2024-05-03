3cc4c319f98e885c0709742f11662f28e7253bc1
DevYesCel <42076081+lucasjaaj@users.noreply.github.com>
2024-05-03 15:02:24 -0300

    att

---
+++ vers 2/README.md
@@ -0,0 +1,2 @@
+# desenvolvimento_hello
+Desenvolvimento site hello 
---
+++ vers 2/assets/script/script.js
@@ -0,0 +1,39 @@
+//Acordeon para aparecer os elementos 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
+
+
+
