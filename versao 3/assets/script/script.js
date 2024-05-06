71b3d1f27d0ab7e1c3f1a22adc42936edd1987e7
DevYesCel <42076081+lucasjaaj@users.noreply.github.com>
2024-05-06 11:18:11 -0300

    att

---
+++ versao 3/assets/script/script.js
@@ -0,0 +1,24 @@
+//Acordeon para aparecer os elementos 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+
+//função para aparecer e esconder redes sociais !
+let el = document.getElementById('iconContainer');
+toggleBtn.addEventListener('click', function () {
+    (el.classList.contains("mostrar") == true) ? el.classList.remove("mostrar") : el.classList.add('mostrar');
+});
+
+
